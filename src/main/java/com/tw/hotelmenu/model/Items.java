7aa2f64ca4864a6292bf572b86aec593a1d60dc5
priyanka <priyanka@priyankas-MacBook-Pro.local>
2022-09-11 13:29:57 +0530

    Removing annotations for code fix.

--- src/main/java/com/tw/hotelmenu/model/Items.java
+++ src/main/java/com/tw/hotelmenu/model/Items.java
@@ -1,15 +1,10 @@
 package com.tw.hotelmenu.model;
 import com.sun.istack.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
 @Table(name="items")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Items {
 
     @Id
@@ -29,9 +24,51 @@ public class Items {
     @Column(name = "price")
     private double price;
 
+    public Items() {
+    }
+
+    public Items(long id, String name, String type, double price) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
     public Items(String name, String type, double price) {
         this.name = name;
         this.type = type;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 }
